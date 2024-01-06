4bc211d68e941c188e60303bfe3ce4dcf00e401c
Sohan-Sunkari <88518634+Sohan-Sunkari@users.noreply.github.com>
2024-01-05 19:58:26 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,80 @@
-var screen_width = 0;
-var screen_height = 0;
-var apple = '';
-var speak_data = '';
-var to_number = '';
+x = 0;
+y = 0;
 
-// Task 2: Load Image
-function preload() {
-    apple = loadImage('apple.png');
+screen_width = 0;
+screen_height = 0;
+
+draw_apple = "";
+
+apple = "";
+speak_data = "";
+to_number = 0;
+
+function preload()
+{
+  apple = loadImage("apple.png");
 }
 
-// Task 3: Inside the recognition.onresult function
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
 recognition.onresult = function(event) {
-    var content = event.results[0][0].transcript;
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
     to_number = Number(content);
-    if (Number.isInteger(to_number)) {
-        document.getElementById('status').innerHTML = "Started drawing apple";
-        var draw_apple = "set";
-    } else {
-        document.getElementById('status').innerHTML = "The speech has not recognized a number";
+    if(Number.isInteger(to_number))
+    {
+      document.getElementById("status").innerHTML = "Started drawing apple "; 
+      draw_apple = "set";
     }
+    else
+    {
+      document.getElementById("status").innerHTML = "The speech has not recognized a number "; 
+    }
+
 }
 
-// Task 4: Inside the setup() function
 function setup() {
-    screen_width = window.innerWidth;
-    screen_height = window.innerHeight;
-    var canvas = createCanvas(screen_width, screen_height - 150);
-    canvas.position(0, 150);
+  screen_width = window.innerWidth;
+  screen_height = window.innerHeight;
+
+  canvas = createCanvas(screen_width, screen_height-150);
+  canvas.position(0,150);
 }
 
-// Task 5: Inside the draw() function
 function draw() {
-    if (draw_apple == "set") {
-        for (var i = 1; i <= to_number; i++) {
-            var x = Math.floor(Math.random() * 700);
-            var y = Math.floor(Math.random() * 400);
-            image(apple, x, y, 50, 50);
-        }
-        document.getElementById('status').innerHTML = to_number + " Apples Drawn";
-        speak_data = to_number + " Apples drawn";
-        speak();
+  if(draw_apple == "set")
+  {
+    for(var i = 1 ; i <= to_number; i++)
+    {
+      x = Math.floor(Math.random() * 700);
+      y = Math.floor(Math.random() * 400);
+      image(apple, x, y, 50, 50);
     }
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    speak_data = to_number + "Apples drawn";
+    speak();
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
 }
